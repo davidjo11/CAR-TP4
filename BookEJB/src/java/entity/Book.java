5d699f20e2aff82e6ec514dc8d5bf7d5d2a4e67b
thibaud <thibaud@Thibaud-PC>
2015-04-14 16:46:50 +0200

    commencement de la 3e partie du TP

---
+++ BookEJB/src/java/entity/Book.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author thibaud
+ */
+@Entity
+@Table(name = "Book")
+@NamedQueries({@NamedQuery(name = "Book.findByAuthor", 
+  query = "SELECT b FROM Book b WHERE b.author = :author"), 
+               @NamedQuery(name = "Book.findByTitle", 
+  query = "SELECT b FROM Book b WHERE b.title = :title"), 
+               @NamedQuery(name = "Book.findByDate", 
+  query = "SELECT b FROM Book b WHERE b.date = :date")})
+public class Book implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private String title;
+    
+    private String author;
+    
+    private int date_p;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public int getDate() {
+        return this.date_p;
+    }
+
+    public void setDate(int date) {
+        this.date_p = date;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (this.title != null ? this.title.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) object;
+        if (this.title.equals(other.title) && this.date_p == other.date_p &&
+                this.author.equals(other.author)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Book[ title=" + this.title + " author=" + this.author + " [" + this.date_p + "]" +" ]";
+    }
+    
+}
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Avancement:
 -----------
 + Etape 1 : Installation [OK]
 + Etape 2 : JSP - Book [OK]
-+ Etape 3 : EJB - En cours ...
++ Etape 3 : EJB - [premiere question OK ? deuxieme question en cours]
